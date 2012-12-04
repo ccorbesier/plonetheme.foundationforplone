5e9bf9606055bc23216ff2a84c3e2faa01d22a63
Catherine Corbesier <ccorbesier@cirb.irisnet.be>
2012-12-04 13:37:03 +0100

    First commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+Introduction
+============
+
+
+This plonetheme is used to test the framwork Foundation with Plone
---
+++ plonetheme/foundationforplone/static/js/theme.js
@@ -0,0 +1,5 @@
+
+function initializeplonethemefoundationforplone(){
+
+}
+jQuery(initializeplonethemefoundationforplone);
